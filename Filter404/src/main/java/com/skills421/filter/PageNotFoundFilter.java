1b1f087109422fc814c50a412f70c50fbe61b1d9
John Dunning <johndunning@mac.com>
2014-07-23 18:58:06 +0100

    Updated to create the content pages
    If a content01.html - content09.html page is requested then it is
    returned if it does exist.
    If it does not exist, then the page is generated and written, and a
    slightly modified content is returned to the user that created the page
    letting them know that the page they are looking at was generated.
    
    Subsequent visits to the page will return the contentxx.html page which
    does not say that it was generated

--- Filter404/src/main/java/com/skills421/filter/PageNotFoundFilter.java
+++ Filter404/src/main/java/com/skills421/filter/PageNotFoundFilter.java
@@ -1,5 +1,6 @@
 package com.skills421.filter;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.servlet.Filter;
@@ -9,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.skills421.filter.wrapper.PageNotFoundResponse;
@@ -19,6 +21,66 @@ import com.skills421.filter.wrapper.PageNotFoundResponse;
 @WebFilter(filterName = "PageNotFoundFilter", urlPatterns = {"/*"})
 public class PageNotFoundFilter implements Filter
 {
+	private void writeFile(String fullpath, String content) throws IOException
+	{
+		System.out.println(fullpath);
+		
+		FileWriter fw = new FileWriter(fullpath);
+		fw.write(content);
+		fw.flush();
+		fw.close();
+	}
+	
+	private String buildPageBody(int contentNo)
+	{
+		StringBuilder htmlContentSB = new StringBuilder();
+		htmlContentSB
+		.append("<h2>Content 0")
+		.append(contentNo)
+		.append("</h2>")
+		.append("<p>")
+		.append("This is the content for page ")
+		.append(contentNo)
+		.append("</p>")
+		.append("<p>");
+		
+		if(contentNo>1)
+		{
+			htmlContentSB
+			.append("<a href='content0")
+			.append(contentNo-1)
+			.append(".html'>");
+		}
+		
+		htmlContentSB.append("previous");
+		
+		if(contentNo>1)
+		{
+			htmlContentSB.append("</a>");
+		}
+		
+		htmlContentSB.append(" - ");
+		
+		if(contentNo<9)
+		{
+			htmlContentSB
+			.append("<a href='content0")
+			.append(contentNo+1)
+			.append(".html'>");
+		}
+		
+		htmlContentSB.append("next");
+		
+		if(contentNo<9)
+		{
+			htmlContentSB.append("</a>");
+		}
+		
+		htmlContentSB
+		.append("</p>");
+		
+		return htmlContentSB.toString();
+	}
 
 	/**
 	 * Default constructor.
@@ -39,16 +101,92 @@ public class PageNotFoundFilter implements Filter
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
 	{
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
-
+				
+		String path = httpRequest.getServletPath();
+		
+		//
+		// -- if we don't have page content01.html - content09.html
+		// -- then process the page using the default mechanism
+		//
+		
+		if(!path.matches("/content0[1-9].html"))
+		{
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		//
+		// -- we have a valid content page so we need to wrap the response
+		// -- incase the page cannot be found and we need to modify the response
+		// -- ourselves on the way back from the Server
+		
 		PageNotFoundResponse pageNotFoundResponse = new PageNotFoundResponse(httpResponse);
 		
+		//
+		// -- pass the request up the chain
+		//
+		
 		chain.doFilter(request, pageNotFoundResponse);
+		
+		//
+		// -- now we are processing the response
+		// -- coming back from the server
+		//
+		
+		//
+		// -- if the page has not been found we need to generate it
+		//
 				
 		if (pageNotFoundResponse.getStatus() == HttpServletResponse.SC_NOT_FOUND)
 		{
-			httpResponse.getWriter().write("404 Error Successfully Intercepted");
+			
+			//
+			// -- determine the content number of the page (01 to 09)
+			//
+			
+			int startIdx = path.indexOf("content")+"content".length();
+			int endIdx = startIdx +2;
+			
+			int contentNo = Integer.parseInt(path.substring(startIdx,endIdx));
+			
+			//
+			// -- build the content and write the page contentxx.html
+			//
+			
+			StringBuilder htmlContentSB = new StringBuilder();
+			htmlContentSB
+			.append("<html><body>")
+			.append(this.buildPageBody(contentNo))
+			.append("</body> </html>");
+			
+			String pagename = String.format("/content0%d.html", contentNo);
+			String fullpath = httpRequest.getServletContext().getRealPath(pagename);
+						
+			this.writeFile(fullpath, htmlContentSB.toString());
+			
+			//
+			// -- return to the customer a slightly different content
+			// -- to that we wrote to contentxx.html
+			// -- this content tells the user the content is GENERATED CONTENT
+			//
+			
+			htmlContentSB = new StringBuilder();
+			htmlContentSB
+			.append("<html><body>")
+			.append(this.buildPageBody(contentNo))
+			.append("<p><strong>GENERATED CONTENT</strong></p>")
+			.append("</body> </html>");
+			
+			//
+			// -- set the response content
+			// -- set the page status from 404 (not found) to 200 (OK)
+			//
+			
+			httpResponse.getWriter().write(htmlContentSB.toString());
 			httpResponse.setStatus(HttpServletResponse.SC_OK);
+			
 		}
 	}
 
